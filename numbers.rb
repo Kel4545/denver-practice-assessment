bca6b117dfe36e758f1c954b9e1972aac51a0ef4
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-06-23 09:34:45 -0600

    Assessment

--- numbers.rb
+++ numbers.rb
@@ -1,1 +1,21 @@
 numbers = [20, 33, 54, 21, 87, 32, 99, 10]
+
+#print the number of elements in the numbers array
+
+p numbers.length
+
+
+#print the largest number in the array
+
+p numbers.max
+
+
+#print the sum of the numbers
+puts numbers.inspect
+counter = 0
+numbers.each do |number|
+  counter += number
+end
+puts
+puts counter
+
